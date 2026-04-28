fb345f48e47a44844159297636c820155943ed40
unknown <aliframadhan672@gmail.com>
2026-04-28 20:12:10 +0700

    fix in console

--- src/app/pages/team-detail/team-detail.page.ts
+++ src/app/pages/team-detail/team-detail.page.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'; // Pastikan ini ada!
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core'; // Tambahkan ElementRef & ViewChild
+import { ActivatedRoute } from '@angular/router';
 import { LigaService } from 'src/app/services/liga.service';
 
 @Component({
   selector: 'app-team-detail',
   templateUrl: './team-detail.page.html',
   styleUrls: ['./team-detail.page.scss'],
-  standalone: false, 
+  standalone: false,
 })
 export class TeamDetailPage implements OnInit {
-    team: any;
-    activeTab: string = 'info';
+  team: any;
+  activeTab: string = 'info';
+
+  // Tambahkan ViewChild untuk menangkap elemen judul di HTML
+  @ViewChild('pageTitle', { read: ElementRef }) pageTitle!: ElementRef;
+
   constructor(
     private route: ActivatedRoute,
     private ligaService: LigaService,
-  ) { }
+  ) {}
 
-ngOnInit() {
+  ngOnInit() {
     const teamId = this.route.snapshot.paramMap.get('id');
     if (teamId) {
       this.team = this.ligaService.getTeamById(teamId);
+    }
+  }
+
+  // Gunakan lifecycle hook Ionic untuk memindahkan fokus
+  ionViewDidEnter() {
+    if (this.pageTitle) {
+      // Fokuskan ke judul halaman agar browser melepas fokus dari halaman Home
+      this.pageTitle.nativeElement.focus();
     }
   }
 }
